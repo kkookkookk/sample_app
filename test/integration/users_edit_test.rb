3d3c15922f705d5f3c51d896b13c3aba7509df40
kousuke okuawa <0930.oku.kou@gmail.com>
2019-01-17 17:04:12 +0900

    Add account activation

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -11,9 +11,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name:  "",
-                                              email: "foo@invalid",
-                                              password:              "foo",
-                                              password_confirmation: "bar" } }
+                              email: "foo@invalid",
+                              password:              "foo",
+                              password_confirmation: "bar" } }
 
     assert_template 'users/edit'
   end
@@ -25,9 +25,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
-                                              email: email,
-                                              password: "",
-                                              password_confirmation: "" } }
+                              email: email,
+                              password: "",
+                              password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
@@ -42,9 +42,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
-                                              email: email,
-                                              password: "",
-                                              password_confirmation: "" } }
+                              email: email,
+                              password: "",
+                              password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
