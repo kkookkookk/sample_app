7bac80564395dfd2361181a98af5d3f0a3bff60d
kousuke okuawa <0930.oku.kou@gmail.com>
2019-01-18 18:01:50 +0900

    heroku addonの再修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'XXXXX.herokuapp.com'
+  host = 'serene-peak-66878.herokuapp.com'
   config_action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
